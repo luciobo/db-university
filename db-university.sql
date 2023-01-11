ce23fe8b1f2e43c42a33210b2eb4813420d29fc3
luciobo <luciobo@icloud.com>
2023-01-11 16:38:52 +0100

    finito spero

--- db-university.sql
+++ db-university.sql
@@ -14,10 +14,16 @@ FROM `students`
 WHERE YEAR (`date_of_birth`) < 1991
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT * 
+FROM `courses`
+WHERE `period`= "I semestre" 
+AND `year` = "1"
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-
+SELECT * 
+FROM `exams` 
+WHERE `date` = "2020-06-20" 
+AND `hour` > '14:00:00'
 
 --6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT * 
@@ -32,3 +38,6 @@ FROM `departments`
 SELECT * 
 FROM `teachers`
 WHERE `phone` IS NULL
+
+
+SELECT * FROM `exams` WHERE `date` = "2020-06-20" AND HOUR(`hour`) = "14:00:00"
