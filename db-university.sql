6937cec8314434353f0c7d01cd1628ce69c32990
luciobo <luciobo@icloud.com>
2023-01-11 16:17:14 +0100

    altri passi in avanti

--- db-university.sql
+++ db-university.sql
@@ -17,7 +17,13 @@ WHERE YEAR (`date_of_birth`) < 1991
 
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+
 --6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = "magistrale"
+
 --7. Da quanti dipartimenti è composta l'università? (12)
 SELECT * 
 FROM `departments`
